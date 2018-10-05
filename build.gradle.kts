dd13dc2aa82086b83aa450b218f6f2c8b8c583a6
Неведомский Андрей Андреевич <AANevedomsky.SBT@sberbank.ru>
2018-10-05 15:07:23 +0500

    add IDEA version and git4idea plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,10 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+val ideaVersion = "182.4892.2"
+intellij {
+    version = System.getenv().getOrDefault("IDEA_VERSION", ideaVersion)
+    pluginName = "GIT skip-worktree"
+    setPlugins("git4idea")
+}
