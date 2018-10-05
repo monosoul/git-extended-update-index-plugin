05d9d4acc6b7ed873cfe015421c98ecf8a14e002
Неведомский Андрей Андреевич <AANevedomsky.SBT@sberbank.ru>
2018-10-05 14:41:32 +0500

    set java version to 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,8 @@ version = "1.0"
 plugins {
     id("org.jetbrains.intellij") version "0.3.11"
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
