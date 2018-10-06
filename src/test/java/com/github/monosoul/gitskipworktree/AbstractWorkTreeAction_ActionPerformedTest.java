71e8d6a748f356f348f370ab5fa54410bcc25ca3
monosoul <kloz.klaud@gmail.com>
2018-10-07 02:12:21 +0500

    introduce test for AbstractWorkTreeAction.actionPerformed()

---
+++ src/test/java/com/github/monosoul/gitskipworktree/AbstractWorkTreeAction_ActionPerformedTest.java
@@ -0,0 +1,203 @@
+package com.github.monosoul.gitskipworktree;
+
+import static com.github.monosoul.gitskipworktree.Util.getRandomSkipWorkTreeCommand;
+import static com.intellij.openapi.application.ApplicationManager.setApplication;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.generate;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomUtils.nextInt;
+import static org.apache.log4j.Level.DEBUG;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import com.intellij.mock.MockApplicationEx;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.ProjectLevelVcsManager;
+import com.intellij.openapi.vcs.actions.VcsContext;
+import com.intellij.openapi.vcs.changes.VcsDirtyScopeManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import git4idea.commands.Git;
+import git4idea.commands.GitCommandResult;
+import git4idea.commands.GitLineHandler;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import lombok.val;
+import org.apache.log4j.Appender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.stubbing.Answer;
+import org.picocontainer.MutablePicoContainer;
+
+class AbstractWorkTreeAction_ActionPerformedTest {
+
+    private static final int LIMIT = 10;
+
+    private MockApplicationEx application;
+    @Mock
+    private Disposable parent;
+    @Mock
+    private MutablePicoContainer picoContainer;
+    @Mock
+    private Git git;
+    @Mock
+    private VcsContext vcsContext;
+    @Mock
+    private Project project;
+    @Mock
+    private ProjectLevelVcsManager vcsManager;
+    @Mock
+    private VcsDirtyScopeManager dirtyScopeManager;
+    @Mock
+    private Function<Entry<VirtualFile, List<VirtualFile>>, GitLineHandler> gitLineHandlerCreator;
+    @Mock
+    private GitLineHandler gitLineHandler;
+    @Mock
+    private GitCommandResult gitCommandResult;
+
+    @SuppressWarnings("unchecked")
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+        application = spy(new MockApplicationEx(parent));
+        setApplication(application, parent);
+
+        doReturn(picoContainer).when(application).getPicoContainer();
+        doReturn(git).when(picoContainer).getComponentInstance(Git.class.getName());
+
+        doReturn(project).when(vcsContext).getProject();
+        doReturn(vcsManager).when(project).getComponent(ProjectLevelVcsManager.class);
+        doReturn(dirtyScopeManager).when(project).getComponent(VcsDirtyScopeManager.class);
+
+        doReturn(gitLineHandler).when(gitLineHandlerCreator).apply(any(Entry.class));
+        doReturn(gitCommandResult).when(git).runCommand(gitLineHandler);
+    }
+
+    @ParameterizedTest
+    @MethodSource("skipWorkTreeCommandStream")
+    void doNotPerformActionIfProjectIsNull(final SkipWorkTreeCommand command) {
+        when(vcsContext.getProject()).thenReturn(null);
+
+        abstractWorkTreeAction(command).actionPerformed(vcsContext);
+
+        verify(vcsContext).getProject();
+        verifyNoMoreInteractions(vcsContext);
+        verifyZeroInteractions(git);
+    }
+
+    @ParameterizedTest
+    @MethodSource("skipWorkTreeCommandStream")
+    void doNothingIfNoRootForFile(final SkipWorkTreeCommand command) {
+        val file = mock(VirtualFile.class);
+
+        when(vcsContext.getSelectedFilesStream()).then((Answer<Stream<VirtualFile>>) i -> Stream.of(file));
+        when(vcsManager.getVcsRootFor(file)).thenReturn(null);
+
+        abstractWorkTreeAction(command).actionPerformed(vcsContext);
+
+        verify(vcsContext).getProject();
+        verify(vcsContext, times(2)).getSelectedFilesStream();
+        verify(vcsManager).getVcsRootFor(file);
+        verify(dirtyScopeManager).fileDirty(file);
+        verifyNoMoreInteractions(vcsContext, vcsManager, dirtyScopeManager);
+        verifyZeroInteractions(git);
+    }
+
+    @ParameterizedTest
+    @MethodSource("virtualFileListAndCommandStream")
+    void executeGitCommandAndLogNothingIfSuccessful(final List<VirtualFile> files, final SkipWorkTreeCommand command) {
+        val root = mock(VirtualFile.class);
+
+        when(vcsContext.getSelectedFilesStream()).then((Answer<Stream<VirtualFile>>) i -> files.stream());
+        when(vcsManager.getVcsRootFor(any(VirtualFile.class))).thenReturn(root);
+        when(gitCommandResult.success()).thenReturn(true);
+
+        abstractWorkTreeAction(command).actionPerformed(vcsContext);
+
+        verify(vcsContext).getProject();
+        verify(vcsContext, times(2)).getSelectedFilesStream();
+        verify(vcsManager, times(files.size())).getVcsRootFor(any(VirtualFile.class));
+        verify(git).runCommand(gitLineHandler);
+        verify(gitCommandResult).success();
+        verify(dirtyScopeManager, times(files.size())).fileDirty(any(VirtualFile.class));
+        verifyNoMoreInteractions(vcsContext, vcsManager, git, gitCommandResult, dirtyScopeManager);
+    }
+
+    @ParameterizedTest
+    @MethodSource("virtualFileListAndCommandStream")
+    void executeGitCommandAndLogErrorIfNotSuccessful(final List<VirtualFile> files, final SkipWorkTreeCommand command) {
+        val root = mock(VirtualFile.class);
+        val captor = ArgumentCaptor.forClass(LoggingEvent.class);
+        val appender = configureAppender();
+        val errorMessage = randomAlphabetic(LIMIT);
+
+        when(vcsContext.getSelectedFilesStream()).then((Answer<Stream<VirtualFile>>) i -> files.stream());
+        when(vcsManager.getVcsRootFor(any(VirtualFile.class))).thenReturn(root);
+        when(gitCommandResult.success()).thenReturn(false);
+        when(gitCommandResult.getErrorOutput()).thenReturn(singletonList(errorMessage));
+
+        abstractWorkTreeAction(command).actionPerformed(vcsContext);
+
+        verify(vcsContext).getProject();
+        verify(vcsContext, times(2)).getSelectedFilesStream();
+        verify(vcsManager, times(files.size())).getVcsRootFor(any(VirtualFile.class));
+        verify(git).runCommand(gitLineHandler);
+        verify(gitCommandResult).success();
+        verify(gitCommandResult).getErrorOutput();
+        verify(dirtyScopeManager, times(files.size())).fileDirty(any(VirtualFile.class));
+        verify(appender).doAppend(captor.capture());
+        verifyNoMoreInteractions(vcsContext, vcsManager, git, gitCommandResult, dirtyScopeManager);
+
+        assertThat(captor.getValue().getMessage()).isEqualTo(errorMessage);
+    }
+
+    private Appender configureAppender() {
+        val appender = mock(Appender.class);
+        val logger = Logger.getLogger(AbstractWorkTreeAction.class);
+        logger.addAppender(appender);
+        logger.setLevel(DEBUG);
+
+        return appender;
+    }
+
+    private AbstractWorkTreeAction abstractWorkTreeAction(final SkipWorkTreeCommand command) {
+        val abstractWorkTreeAction = spy(new TestAbstractWorkTreeActionImpl(command));
+        doReturn(gitLineHandlerCreator).when(abstractWorkTreeAction).gitLineHandlerCreator(project);
+
+        return abstractWorkTreeAction;
+    }
+
+    private static Stream<Arguments> virtualFileListAndCommandStream() {
+        return generate(() -> (Arguments) () ->
+                new Object[]{
+                        virtualFileStream().limit(nextInt(1, LIMIT)).collect(toList()),
+                        getRandomSkipWorkTreeCommand()
+                }
+        ).limit(LIMIT);
+    }
+
+    private static Stream<VirtualFile> virtualFileStream() {
+        return generate(() -> mock(VirtualFile.class)).limit(LIMIT);
+    }
+
+    private static Stream<SkipWorkTreeCommand> skipWorkTreeCommandStream() {
+        return generate(Util::getRandomSkipWorkTreeCommand).limit(LIMIT);
+    }
+}
