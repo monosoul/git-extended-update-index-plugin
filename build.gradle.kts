9cd384e8a20903e99aeef325e2df29f4658fa6da
monosoul <kloz.klaud@gmail.com>
2018-10-06 20:29:31 +0500

    add Junit 5.3.1, Mockito and AssertJ dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,15 +13,30 @@ java {
 val ideaVersion = "182.4892.2"
 intellij {
     version = System.getenv().getOrDefault("IDEA_VERSION", ideaVersion)
-    pluginName = "GIT skip-worktree"
+    pluginName = "GIT skip-worktree plugin"
     setPlugins("git4idea")
 }
 
 dependencies {
     val lombokDependency = "org.projectlombok:lombok:1.18.2"
+    val junitVersion  = "5.3.1"
+
     annotationProcessor(lombokDependency)
     compileOnly(lombokDependency)
     testCompileOnly(lombokDependency)
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testCompile("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+
+    testCompile("org.assertj:assertj-core:3.11.1")
+    testCompile("org.mockito:mockito-core:2.23.0")
+}
+
+tasks {
+    "test"(Test::class) {
+        useJUnitPlatform()
+    }
 }
 
 repositories {
